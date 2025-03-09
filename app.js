c02d7fb1083f05844c4bd7455f63145e7afc0d7f
skyl3-r <skyler.ng.nyz@gmail.com>
2025-03-09 16:39:24 +0800

    Add ability to read query params

--- app.js
+++ app.js
@@ -10,6 +10,20 @@ app.get('/', (req, res) => {
 })
 
 app.get('/users', (req, res) => {
+    // get query params
+    // if params are invalid, will be changed to default
+    const min = parseFloat(req.query.min) || 0.0;
+    const max = parseFloat(req.query.max) || 4000.0;
+    const offset = parseInt(req.query.offset) || 0;
+    const limit = parseInt(req.query.limit) || null;
+    var sort = req.query.sort;
+    if (!['NAME', 'SALARY'].includes(sort)) {
+        sort = null;
+    }
+
+    // console.log(min, max, offset, limit, sort)
+
+    // read data.csv
     const result = [];
     fs.createReadStream("./data.csv")
         .pipe(csvParser({
