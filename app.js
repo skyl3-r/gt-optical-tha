f33c99cbd3da0b09697938884346e89ad3197deb
skyl3-r <skyler.ng.nyz@gmail.com>
2025-03-09 16:05:55 +0800

    Add functionality to read and return local data.csv file

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
-const express = require('express')
-const app = express()
-const port = 4321
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const fs = require('fs');
+const csvParser = require('csv-parser');
 
 app.get('/', (req, res) => {
-    res.send("express tutorial!")
+    res.send(`<a href='http://localhost:${port}/users'>Go to users page</a>`);
+})
+
+app.get('/users', (req, res) => {
+    const result = [];
+    fs.createReadStream("./data.csv")
+        .pipe(csvParser({
+            skipLines: 1,
+            headers: ['name', 'salary']
+        }))
+        .on("data", (data) => {
+            // console.log(data['salary']);
+            data['salary'] = parseFloat(data['salary'])
+            result.push(data);
+        })
+        .on("end", () => {
+            res.json({"results": result});
+        })
+})
+
+app.post('/upload', (req, res) => {
+
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`App is running at http://localhost:${port}/users`)
 })
